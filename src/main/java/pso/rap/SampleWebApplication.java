e83ecbe4cfcb54825d60ccac0d797f82cffdf5eb
Biju Kunjummen <Biju.Kunjummen@gmail.com>
2014-06-09 23:28:09 -0400

    WIP With sqlfire

--- src/main/java/pso/rap/SampleWebApplication.java
+++ src/main/java/pso/rap/SampleWebApplication.java
@@ -1,9 +1,14 @@
 package pso.rap;
 
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 
 @Configuration
@@ -13,5 +18,22 @@ public class SampleWebApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SampleWebApplication.class, args);
+	}
+
+
+	@Value("${spring.datasource.url}")
+	private String dataSourceUrl;
+
+	@Value("${spring.driver.class}")
+	private String driverClassName;
+
+	@Bean
+	public DataSource dataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName(driverClassName);
+		dataSource.setUrl(dataSourceUrl);
+		dataSource.setUsername("app");
+		dataSource.setPassword("app");
+		return dataSource;
 	}
 }
